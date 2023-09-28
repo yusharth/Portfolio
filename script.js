a233a92a437034dd2472c0951d3f17d74b9267ff
whyusharthwhy <yusharth.singh@yashdoamail.com>
2023-09-28 16:29:55 +0530

    changed profile pic and added some other more things

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ import fontawesome from '@fortawesome/fontawesome'
 import brands from '@fortawesome/fontawesome-free-brands'
 
 fontawesome.library.add(brands)
+
+new WOW().init();
+
+
+  	/* AUTHOR LINK */
+     $('.about-me-img').hover(function(){
+            $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+        }, function(){
+            $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+        });
